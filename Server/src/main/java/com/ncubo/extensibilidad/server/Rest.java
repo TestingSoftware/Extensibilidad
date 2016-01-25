101429aff413fe759a711cf91e541d4904c9949c
Dalaian <dalaian@outlook.com>
2016-01-25 14:03:33 -0600

    En el ticket #2 hice que se le envien mensajes tanto a la cola de pedidos y de productos

---
+++ Server/src/main/java/com/ncubo/extensibilidad/server/Rest.java
@@ -0,0 +1,36 @@
+package com.ncubo.extensibilidad.server;
+
+import javax.jms.JMSException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class Rest {
+
+	final private String NOMBRE_COLA_PRODUCTO = "PRODUCTO";
+	final private String NOMBRE_COLA_PEDIDO = "PEDIDO";
+	@Autowired
+	private ConectorActiveMQ conectorActiveMQ;
+
+	@RequestMapping(value="/producto",  method=RequestMethod.POST)
+	public @ResponseBody void crearProducto(
+		@RequestBody String body) throws JMSException {
+		
+		conectorActiveMQ.enviarMensaje(NOMBRE_COLA_PRODUCTO, body);
+	}
+	
+	@RequestMapping(value="/pedido",  method=RequestMethod.POST)
+	public @ResponseBody void crearPedido(
+			@RequestBody String body) throws JMSException {
+		
+		conectorActiveMQ.enviarMensaje(NOMBRE_COLA_PEDIDO, body);
+	
+	}
+
+	
+}
