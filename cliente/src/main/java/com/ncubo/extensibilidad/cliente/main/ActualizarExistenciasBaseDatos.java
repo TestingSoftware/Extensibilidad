966e714dab5dea08677657bfdfda06da34ea4322
cooper15 <v.centeno15@gmail.com>
2016-02-11 09:55:47 -0600

    Closed #12 Se manda a la cola los productos de Nimbus que se actualizaron.

--- cliente/src/main/java/com/ncubo/extensibilidad/cliente/main/ActualizarExistenciasBaseDatos.java
+++ cliente/src/main/java/com/ncubo/extensibilidad/cliente/main/ActualizarExistenciasBaseDatos.java
@@ -5,27 +5,53 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jms.JMSException;
+
+import com.google.gson.Gson;
 import com.ncubo.extensibilidad.cliente.dao.MapeoDao;
+import com.ncubo.extensibilidad.cliente.librerias.Existencias;
 import com.ncubo.extensibilidad.cliente.librerias.IdNimbus;
 import com.ncubo.extensibilidad.cliente.librerias.Mapeo;
 
 public class ActualizarExistenciasBaseDatos 
 {
-
-	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
-		actualizarExistencias();
+	private static int numProductosInsertar;
+	public int getNumProductosInsertar() {
+		return numProductosInsertar;
 	}
-
-	private static void actualizarExistencias() throws IOException, SQLException, ClassNotFoundException {
-		MapeoDao mapeoDao = new MapeoDao();
+	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException, JMSException 
+	{
+		if(!args.equals(null))
+		{
+			Existencias existencias = new Existencias();
+			MapeoDao mapeoDao = new MapeoDao();
+			List<Mapeo> existenciasPorInsertar = new ArrayList<Mapeo>();
+			existenciasPorInsertar = productosPorInsertar();
+			
+			for(Mapeo producto : existenciasPorInsertar)
+			{
+				mapeoDao.insertar(producto);
+			}
+			// Se envian todos los productos actualizados en un solo string a Nimbus
+			Gson gson = new Gson();
+			String gsonObject = gson.toJson(existenciasPorInsertar);
+			existencias.actualizar(gsonObject);
+			//TODO se pueden enviar los productos uno por uno, CONSULTAR 
+		}
+		else
+		{
+			// Para el TestCase
+			List<Mapeo> existenciasPorInsertar = new ArrayList<Mapeo>();
+			existenciasPorInsertar = productosPorInsertar();
+			numProductosInsertar = existenciasPorInsertar.size();
+		}
+	}
+	private static List<Mapeo> productosPorInsertar() throws ClassNotFoundException, IOException, SQLException
+	{
 		List<Mapeo> existenciasDb = new ArrayList<Mapeo>();
-		List<Mapeo> existenciasPorInsertar = new ArrayList<Mapeo>();
-		
+		MapeoDao mapeoDao = new MapeoDao();
 		existenciasDb = (ArrayList <Mapeo>)mapeoDao.obtener();
 		IdNimbus idNimbus = new IdNimbus();
-		existenciasPorInsertar = idNimbus.actualizarEnBd(existenciasDb);
-		
-		for(Mapeo producto : existenciasPorInsertar)
-			mapeoDao.insertar(producto);
+		return idNimbus.actualizarEnBd(existenciasDb);
 	}
 }
--- cliente/src/test/java/com/ncubo/extensibilidad/cliente/launcher/ConsumirProductosNimbus.java
+++ cliente/src/test/java/com/ncubo/extensibilidad/cliente/launcher/ConsumirProductosNimbus.java
@@ -5,6 +5,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jms.JMSException;
+
 import org.junit.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -14,6 +16,7 @@ import com.ncubo.extensibilidad.cliente.dao.MapeoDao;
 import com.ncubo.extensibilidad.cliente.dao.Persistencia;
 import com.ncubo.extensibilidad.cliente.librerias.IdNimbus;
 import com.ncubo.extensibilidad.cliente.librerias.Mapeo;
+import com.ncubo.extensibilidad.cliente.main.ActualizarExistenciasBaseDatos;
 
 public class ConsumirProductosNimbus extends Testcase
 {
@@ -26,17 +29,11 @@ public class ConsumirProductosNimbus extends Testcase
 		executeDBScripts("src/test/resources/ConsumirProductosNimbus.sql", ejecutor);
 	}
 	 @Test
-	 public void consumir() throws ClassNotFoundException, IOException, SQLException
+	 public void consumir() throws ClassNotFoundException, IOException, SQLException, JMSException
 	 {
-		 MapeoDao mapeoDao = new MapeoDao();
-		List<Mapeo> existenciasDb = new ArrayList<Mapeo>();
-		List<Mapeo> existenciasPorInsertar = new ArrayList<Mapeo>();
-			
-		existenciasDb = (ArrayList <Mapeo>)mapeoDao.obtener();
-		IdNimbus idNimbus = new IdNimbus();
-		existenciasPorInsertar = idNimbus.actualizarEnBd(existenciasDb);
-		
-		Assert.assertEquals(10, existenciasPorInsertar.size());
-			
+		String [] test = {"1"};
+		ActualizarExistenciasBaseDatos.main(test);
+		ActualizarExistenciasBaseDatos actualizar = new ActualizarExistenciasBaseDatos();
+		Assert.assertEquals(9,actualizar.getNumProductosInsertar());
 	 }
 }
